14892918d345577ac1a4483848614d39f1053e2b
Ryan Karpeles <ryan.karpeles@gmail.com>
2013-05-16 06:38:52 -0500

    Initial commit

---
+++ prayer-calendar.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title></title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width">
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/style.css">
+	
+	<script src="js/jquery-1.8.3.min.js"></script>
+	<script src="js/jquery.equalheights.js"></script>
+	<script>
+		$(document).ready(function() {
+		$(".calendar").equalHeights(100,50);
+	});
+	</script>
+</head>
+<body>
+	<!--[if lt IE 7]>
+		<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+	<![endif]-->
+	
+	<h1>Daily Prayer Calendar</h1>
+
+	<div class="calendar">
+		<ul>
+		  <li><div class="number">1</div><div class="title">Know God's Love</div>Know the breadth, depth, height, and width of God's love.<div class="reference">Eph. 3:17&ndash;19</div></li>
+			<li><div class="number">2</div><div class="title">Abound in Hope</div>Be filled with joy and peace in believing, so that you may "abound in hope."<div class="reference">Romans 15:13</div></li>
+			<li><div class="number">3</div><div class="title">Taste Jesus' Love</div>"Let him kiss me with the kisses of his mouth! For your love is better than wine."<div class="reference">Song of Sol. 1:2</div></li>
+			<li><div class="number">4</div><div class="title">Spirit of Revelation</div>"Give you the Spirit of wisdom and of revelation in the knowledge of him."<div class="reference">Eph. 1:17</div></li>
+			<li><div class="number">5</div><div class="title">Filled with Jesus</div>"Come, everyone who thirsts, comes to the waters; and he who has no money, come, buy and eat!"<div class="reference">Isaiah 55:1</div></li>
+			<li><div class="number">6</div><div class="title">Know True Identity</div>"Because you are precious in my eyes, and honored, and I love you."<div class="reference">Isaiah 43:3</div></li>
+			<li><div class="number">7</div><div class="title">Bless the Lord</div>"Bless the Lord, O my soul, and all that is within me, bless his holy name!"<div class="reference">Psalm 103:1</div></li>
+		</ul>
+	</div>
+
+	<div class="calendar">
+		<ul>
+			<li><div class="number">8</div><div class="title">Thanksgiving</div>"Oh give thanks to the Lord, for he is good; for his steadfast love endures forever!"<div class="reference">1 Chron. 16:34</div></li>
+			<li><div class="number">9</div><div class="title">Protection &amp; Deliverance</div>"Because he holds fast to me in love, I will deliver him; I will protect him, because he knows my name."<div class="reference">Psalm 91:14</div></li>
+			<li><div class="number">10</div><div class="title">Strength in Him</div>"Finally, be strong in the Lord and in the strength of his might."<div class="reference">Eph. 6:10</div></li>
+			<li><div class="number">11</div><div class="title">Bold Witness</div>"That words may be given to me in opening my mouth boldly to proclaim the mystery of the gospel,"<div class="reference">Eph 6:19</div></li>
+			<li><div class="number">12</div><div class="title">Faith to Prophecy</div>"Let us use them [gifts]: if prophecy, in proportion to our faith."<div class="reference">Romans 12:6</div></li>
+			<li><div class="number">13</div><div class="title">His Power</div>"May you be strengthened with all power, according to his glorious might, for all endurance and patience with joy."<div class="reference">Col. 1:11</div></li>
+			<li><div class="number">14</div><div class="title">Knowledge of Christ</div>"To reach all the riches [...] of understanding and the knowledge of God's mystery, which is Christ."<div class="reference">Col. 2:2&ndash;3</div></li>
+		</ul>
+	</div>
+
+	<div class="calendar">
+		<ul>
+			<li><div class="number">15</div><div class="title">God&rsquo;s Help to Train</div>"Train up a child in the way he should go; even when he is old he will not depart from it."<div class="reference">Prov. 22:6</div></li>
+			<li><div class="number">16</div><div class="title">Train Younger Women</div>"Train the young women" in love, self-control, purity, diligence, godliness.<div class="reference">Titus 2:4&ndash;5</div></li>
+			<li><div class="number">17</div><div class="title">Make Disciples</div>Go and make disciples of all nations, baptizing them, and teaching to obey Jesus&rsquo; commandments.<div class="reference">Matt 28:19&ndash;20</div></li>
+			<li><div class="number">18</div><div class="title">Love</div>"verse here"<div class="reference">Verse 3:10</div></li>
+			<li><div class="number">19</div><div class="title">Love</div>"verse here"<div class="reference">Verse 3:10</div></li>
+			<li><div class="number">20</div><div class="title">Love</div>"verse here"<div class="reference">Verse 3:10</div></li>
+			<li><div class="number">21</div><div class="title">Love</div>"verse here"<div class="reference">Verse 3:10</div></li>
+		</ul>
+	</div>
+
+	<div class="calendar">
+		<ul>
+			<li><div class="number">22</div><div class="title">Love</div>"verse here"<div class="reference">Verse 3:10</div></li>
+			<li><div class="number">23</div><div class="title">Love</div>"verse here"<div class="reference">Verse 3:10</div></li>
+			<li><div class="number">24</div><div class="title">Love</div>"verse here"<div class="reference">Verse 3:10</div></li>
+			<li><div class="number">25</div><div class="title">Love</div>"verse here"<div class="reference">Verse 3:10</div></li>
+			<li><div class="number">26</div><div class="title">Love</div>"verse here"<div class="reference">Verse 3:10</div></li>
+			<li><div class="number">27</div><div class="title">Love</div>"verse here"<div class="reference">Verse 3:10</div></li>
+			<li><div class="number">28</div><div class="title">Love</div>"verse here"<div class="reference">Verse 3:10</div></li>
+		</ul>
+	</div>
+
+	<div class="calendar">
+		<ul>
+			<li><div class="number">29</div><div class="title">Love</div>"verse here"<div class="reference">Verse 3:10</div></li>
+			<li><div class="number">30</div><div class="title">Love</div>"verse here"<div class="reference">Verse 3:10</div></li>
+			<li><div class="number">31</div><div class="title">Love</div>"verse here"<div class="reference">Verse 3:10</div></li>			
+		</ul>
+	</div>
+	
+</body>
+</html>
